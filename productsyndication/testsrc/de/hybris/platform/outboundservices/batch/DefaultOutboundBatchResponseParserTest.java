cb020714a28165b98f7e7bb33711246a7d64138f
Wolanski, Pawel <pawel.wolanski@sap.com>
2025-04-03 14:00:06 +0200

    bug(11): issues with spring bean references

--- productsyndication/testsrc/de/hybris/platform/outboundservices/batch/DefaultOutboundBatchResponseParserTest.java
+++ productsyndication/testsrc/de/hybris/platform/outboundservices/batch/DefaultOutboundBatchResponseParserTest.java
@@ -14,29 +14,30 @@ import de.hybris.platform.odata2services.converter.HttpHeaderToMapConverter;
 @UnitTest
 public class DefaultOutboundBatchResponseParserTest {
 
-	String body = "--batch_300b476e-f92b-4622-b628-3c5e0bee69f3\r\n" +
-			"Content-Type: application/http\r\n" +
-			"Content-Transfer-Encoding: binary\r\n" +
-			"\r\n" +
-			"HTTP/1.1 400 Bad Request\r\n" +
-			"DataServiceVersion: 2.0\r\n" +
-			"Location: 300427245|Online|apparelProductCatalog\r\n" +
-			"Content-Type: application/json\r\n" +
-			"Content-Length: 127\r\n" +
-			"\r\n" +
-			"{\"Required_field_error\":\"Validation unsuccessful -  ean,mpn values missing for product 300427245|Online|apparelProductCatalog\"}\r\n" +
-			"--batch_300b476e-f92b-4622-b628-3c5e0bee69f3\r\n" +
-			"Content-Type: application/http\r\n" +
-			"Content-Transfer-Encoding: binary\r\n" +
-			"\r\n" +
-			"HTTP/1.1 400 Bad Request\r\n" +
-			"DataServiceVersion: 2.0\r\n" +
-			"Location: 300427249|Online|apparelProductCatalog\r\n" +
-			"Content-Type: application/json\r\n" +
-			"Content-Length: 127\r\n" +
-			"\r\n" +
-			"{\"Required_field_error\":\"Validation unsuccessful -  ean,mpn values missing for product 300427249|Online|apparelProductCatalog\"}\r\n" +
-			"--batch_300b476e-f92b-4622-b628-3c5e0bee69f3--";
+	String body = """
+--batch_300b476e-f92b-4622-b628-3c5e0bee69f3\r
+Content-Type: application/http\r
+Content-Transfer-Encoding: binary\r
+\r
+HTTP/1.1 400 Bad Request\r
+DataServiceVersion: 2.0\r
+Location: 300427245|Online|apparelProductCatalog\r
+Content-Type: application/json\r
+Content-Length: 127\r
+\r
+{"Required_field_error":"Validation unsuccessful -  ean,mpn values missing for product 300427245|Online|apparelProductCatalog"}\r
+--batch_300b476e-f92b-4622-b628-3c5e0bee69f3\r
+Content-Type: application/http\r
+Content-Transfer-Encoding: binary\r
+\r
+HTTP/1.1 400 Bad Request\r
+DataServiceVersion: 2.0\r
+Location: 300427249|Online|apparelProductCatalog\r
+Content-Type: application/json\r
+Content-Length: 127\r
+\r
+{"Required_field_error":"Validation unsuccessful -  ean,mpn values missing for product 300427249|Online|apparelProductCatalog"}\r
+--batch_300b476e-f92b-4622-b628-3c5e0bee69f3--""";
 
 
 	de.hybris.platform.odata2services.converter.HttpHeaderToMapConverter headerToMapConverter = new HttpHeaderToMapConverter();
@@ -46,7 +47,7 @@ public class DefaultOutboundBatchResponseParserTest {
 	DefaultOutboundBatchResponseParser testObj = new DefaultOutboundBatchResponseParser(converter);
 
 	@Test(expected = NullPointerException.class)
-	public void testCurrentState() {
+	public void test_content_type_lowercase_olingo_internal_error_related_to_case_sensitivity_of_http_headers() {
 		HttpHeaders headers = new HttpHeaders();
 
 		headers
@@ -65,7 +66,7 @@ public class DefaultOutboundBatchResponseParserTest {
 	}
 
 	@Test
-	public void testParseMultiPartResponse() {
+	public void test_content_type_camelcase_olingo_internal_error_related_to_case_sensitivity_of_http_headers() {
 		HttpHeaders headers = new HttpHeaders();
 
 		headers
